d154c9e88ab666bf299b2aabe0612a15c364e6c1
oona <osarmola@edu.lapinamk.fi>
2024-05-30 23:12:15 +0300

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,63 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import SpamHamPredictor from './components/SpamHam.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <SpamHamPredictor />
+  </div>
 </template>
+
+<script>
+import SpamHamPredictor from './components/SpamHamPredictor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SpamHamPredictor
+  }
+};
+</script>
+<style>
+body{
+    padding: 20px;
+    margin: 0;
+    background-color:bisque;
+}
+.container {
+    max-width: 600px;
+    margin: auto;
+    background-color: #ccc;
+    padding: 20px;
+    box-shadow: 0 2px 4px rgba(0,0,0,0);
+}
+input[type="text"]{
+    width: calc(100% - 100px);
+    padding: 10px;
+    margin-right: 10px;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+button {
+    width: 90px;
+    padding: 10px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+}
+.sentence {
+    border-left: 5px solid #ccc;
+}
+.ham {
+    border-left-color: #4CAF50;
+}
+.spam{
+    border-left-color: #F44336;
+}
+</style>
 
 <style scoped>
 header {
